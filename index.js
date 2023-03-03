d8d79302bff5e29e9756f87a503b4fd2339048d6
Tejasvee Dighe <tejasveedighe@gmail.com>
2023-03-03 10:54:50 +0530

    Basic connect and disconnect event of socket

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
+const app = require("express")();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+	console.log("a user connected");
+	socket.on("disconnect", () => {
+		console.log("user disconnected");
+	});
 });
 
 server.listen(3000, () => {
